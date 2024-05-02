0c401f01d458881ad933e2201d87505d0b4ab97c
James <jdowns06@gmail.com>
2024-05-02 15:43:38 -0500

    Point DB to local datastore and add document templates

--- portfolio_backend/documents/views.py
+++ portfolio_backend/documents/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Document
+from .forms import DocumentForm  # Create this form later
 
-# Create your views here.
+def document_list(request):
+    documents = Document.objects.all()
+    return render(request, 'documents/document_list.html', {'documents': documents})
+
+def document_create(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('document_list')  # Name your list view accordingly
+    else:
+        form = DocumentForm()
+    return render(request, 'documents/document_create.html', {'form': form})
+
+def document_detail(request, pk):
+    document = Document.objects.get(pk=pk)
+    return render(request, 'documents/document_detail.html', {'document': document})
--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'portfolio_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Looks for project-level 'templates'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'portfolio_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.datastore',
+        'NAME': '/portfolio-422119',
+        # Optionally, if you need to specify host/port (usually for local development)
+        'HOST': '127.0.0.1', 
+        'PORT': '8000',  
     }
 }
 
