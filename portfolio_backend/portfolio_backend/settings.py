9378639ae4ef1d1568d51b8a8cbfaa69b10ee89d
James <jdowns06@gmail.com>
2024-05-02 16:09:07 -0500

    Add app engine file

--- portfolio_backend/portfolio_backend/settings.py
+++ portfolio_backend/portfolio_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,7 @@ DATABASES = {
         'NAME': '/portfolio-422119',
         # Optionally, if you need to specify host/port (usually for local development)
         'HOST': '127.0.0.1', 
-        'PORT': '8000',  
+        'PORT': '8081',  
     }
 }
 
